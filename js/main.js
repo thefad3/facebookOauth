73a8a81d2485afccc128d5a091a3b245bdd1c020
Chris Lynch (vVv F R 0 O T x) <thefad3@fullsail.edu>
2015-06-13 17:01:15 +0000

    Start Token Authentication

---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.fbAsyncInit = function() {
+
+
+    FB.init({
+        appId      : '1492538204340357',
+        xfbml      : true,
+        version    : 'v2.3',
+    });
+
+    facebookLogin = function() {
+        FB.login(function (response) {
+            if (response.authResponse) {
+                console.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function (response) {
+                    console.log('Good to see you, ' + response.name + '.');
+                    console.log(response);
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        });
+    };
+
+
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            console.log(response);
+            // the user is logged in and has authenticated your
+            // app, and response.authResponse supplies
+            // the user's ID, a valid access token, a signed
+            // request, and the time the access token
+            // and signed request each expire
+            var uid = response.authResponse.userID;
+            var accessToken = response.authResponse.accessToken;
+        } else if (response.status === 'not_authorized') {
+            // the user is logged in to Facebook,
+            // but has not authenticated your app
+        } else {
+            // the user isn't logged in to Facebook.
+        }
+    });
+
+    FB.api(
+        "1019658694718581?fields=albums",
+        function (response) {
+            if (response && !response.error) {
+               //console.log(response);
+            }
+        }
+    );
+};
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
